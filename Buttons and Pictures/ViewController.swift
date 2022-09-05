b469ae2f3d5ac5f2815566f5d12ecd92f808ed41
Ian Waddington <iwaddo@icloud.com>
2022-09-05 08:32:36 +0100

    V0.1 before trying to fix the constraints

--- Buttons and Pictures/ViewController.swift
+++ Buttons and Pictures/ViewController.swift
@@ -8,12 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func jillButtonPressed(_ sender: UIButton) {
+        imageView.image = UIImage(named: "jill")
+    }
+    @IBAction func ianButtonPressed(_ sender: UIButton) {
+        imageView.image = UIImage(named: "ian")
+    }
+    @IBAction func adamButtonPressed(_ sender: UIButton) {
+        imageView.image = UIImage(named: "adam")
+    }
+    @IBAction func emilyButtonPressed(_ sender: UIButton) {
+        imageView.image = UIImage(named: "emily")
+    }
+    
+    
 }
 
